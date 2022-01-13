b922ff1fba4bd83f2ec7bb402afee2646194ce09
Martha <yuki@yukiA.localdomain>
2022-01-13 03:36:48 +0200

    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # StartThingy
 My terminal start thingy that makes the opening of a terminal look *NICE*, tho its made by me, Yuki
+# How to make it work
+If you want it to work on you have to fist compile it with
+``` c++ code.cpp -o StartThingy ```
+and move the executable resulted to a place where it wont be deleted by mistake and its not obstructive (i for example have a folder in home specifically for random executables).
+
+Afterwards add ```/PATH/TO/EXECUTABLE/StartThingy``` to the initialization file of your specific shell
+
+Reminder that the character ~ at the start of a path is the shell's way to represent your user's path in a small and universal way
+
+Examples for shells (if the first command doesnt work DO NOT run it twice, it just appends the path to your exec to your shell's configuration file)
+## Bash
+`printf %s "/PATH/TO/EXECUTABLE/StartThingy" >> ~/.bashrc`
+or just add the path to the executable in some empty space on a new line to `~/.bashrc`
+## Fish
+`printf %s "/PATH/TO/EXECUTABLE/StartThingy" >> ~/.config/fish/config.fish`
+or just add the path to the executable in some empty space on a new line to `~/.config/fish/config.fish`
+
+# Thingies
+## Trans flag
+Trans Flag
+## LGBT flag
+Classic LGBT flag
+## Cooties
+      #####      | 
+      #   #      | 
+      #####      | This computer is opperated by a TGirl!
+        #        | You got cooties now
+      #####      | 
+        #        | 
--- code.cpp
+++ code.cpp
@@ -1,14 +1,49 @@
-#include<iostream>
+/*
+	This code is made by Martha (@YukiZensho on github & twitter)
+	I would be surprised if anyone would ever be able to get a bug on this app, but if you do hmu on twitter
+	My official email address is contact@martha.link
+*/
+#include<stdio.h>
+#include <ctime>
+#include <chrono>
 
+#include <iostream>
+void trans(){
+	 printf("\033[48;2;91;206;250m                 \033[0m\n");//light blue
+	printf("\033[48;2;245;169;184m                 \033[0m\n");//pink
+	printf("\033[48;2;255;255;255m                 \033[0m\n");//white
+	printf("\033[48;2;245;169;184m                 \033[0m\n");//pink
+	 printf("\033[48;2;91;206;250m                 \033[0m\n");//light blue
+}
+
+void lgbt(){
+	 printf("\033[48;2;228;3;3m                 \033[0m\n");//red
+	printf("\033[48;2;255;140;0m                 \033[0m\n");//orange
+	printf("\033[48;2;255;237;0m                 \033[0m\n");//yellow
+	printf("\033[48;2;0;128;38m                 \033[0m\n");//green
+	 printf("\033[48;2;0;77;255m                 \033[0m\n");//blue
+	 printf("\033[48;2;177;7;135m                 \033[0m\n");//violet
+}
+
+void girl(){
+	printf("      #####      | \n");
+	printf("      #   #      | \n");
+	printf("      #####      | This computer is opperated by a TGirl!\n");
+	printf("        #        | You got cooties now\n");
+	printf("      #####      | \n");
+	printf("        #        | \n");
+}
 int main() {
-	 printf("\033[48;2;91;206;250m                 \033[0m\n");
-	printf("\033[48;2;245;169;184m                 \033[0m\n");
-	printf("\033[48;2;255;255;255m                 \033[0m\n");
-	printf("\033[48;2;245;169;184m                 \033[0m\n");
-	 printf("\033[48;2;91;206;250m                 \033[0m\n");
-
-	//printf("\033[41maa\n");
-	//printf("\033[47maa\n");
-	//printf("\033[41maa\n");
-	//printf("\033[46maa\n");
+	
+	
+    const auto p1 = std::chrono::system_clock::now();
+	int t =std::chrono::duration_cast<std::chrono::seconds>(p1.time_since_epoch()).count();
+	//using epoch timestamp to get sufficient randomness
+	switch(t%3){
+		case 0:trans();break;
+		case 1:lgbt();break;
+		case 2:girl();break;
+		default: printf("a");break;
+	}
+	
 }
